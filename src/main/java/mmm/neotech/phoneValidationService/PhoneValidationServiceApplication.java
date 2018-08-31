8f8af74bd0b9f72bbf5919b7dc7d65cf469a5b84
MMM <mmaksmas@gmail.com>
2018-08-31 21:37:03 +0300

    Initial commit

---
+++ src/main/java/mmm/neotech/phoneValidationService/PhoneValidationServiceApplication.java
@@ -0,0 +1,12 @@
+package mmm.neotech.phoneValidationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneValidationServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/mmm/neotech/phoneValidationService/PhoneValidationServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package mmm.neotech.phoneValidationService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhoneValidationServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
